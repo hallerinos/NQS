4893bc595add1f8baf7b4549b1daa91f71a0e4c6
Andreas Haller <andreas.haller@uni.lu>
2025-09-10 13:35:45 +0200

    removed redundant variables from MCBlock constructor,  make samples slightly more independent by restarting a number of draw_next iterations: n_res

--- energies/TFIM.py
+++ energies/TFIM.py
@@ -6,6 +6,12 @@ def local_energy(wf: RBM, spin_vector: torch.Tensor, J=-1, h=-1):
     interactions = spin_vector[1:] @ spin_vector[:-1]
     interactions += spin_vector[0] * spin_vector[-1]  # pbc
 
+    zeeman_term = torch.zeros_like(interactions)
+    # # sequential adding energy of spin flips (less memory, more time)
+    # for i in range(spin_vector.shape[0]):
+    #     spin_vector_f = spin_vector.clone()
+    #     spin_vector_f[i] *= -1
+    #     zeeman_term += wf.probratio(spin_vector_f, spin_vector)
     # Create copies of original configuration and a batch of flipped configurations
     spin_vector_r = spin_vector.repeat(len(spin_vector), 1)
     flipped_spins = spin_vector.repeat(len(spin_vector), 1)
--- vmc/iterator.py
+++ vmc/iterator.py
@@ -9,12 +9,11 @@ torch._dynamo.config.suppress_errors = True
 
 # @torch.compile(fullgraph=False)
 class MCBlock:
-    def __init__(self, wf, n_block, verbose=0, local_energy=lambda x, y: 1):
+    def __init__(self, wf, n_block, local_energy=lambda x, y: 1):
         self.OK = torch.zeros(n_block, wf.n_param, dtype=wf.dtype, device=wf.device)
         self.samples = 2.0*torch.randint(0, 2, (n_block, wf.n_spins), dtype=wf.dtype, device=wf.device) - 1.0
         self.EL = torch.zeros(n_block, dtype=wf.dtype, device=wf.device)
         self.local_energy = local_energy
-        # self.sample_block(wf, n_block, verbose)
 
     # @torch.compile(fullgraph=False)
     def sample_block(self, wf, n_block, verbose=0, n_discard=2**4, n_iter=2**4):
@@ -45,13 +44,14 @@ class MCBlock:
             # ic(torch.norm(self.OK[n, :] - check))
 
     @torch.compile(fullgraph=False)
-    def bsample_block(self, wf, n_iter=2**4):
-        bdraw_next = torch.compile(torch.vmap(lambda x: draw_next(wf, x, n_flip=1, n_iter=n_iter), randomness='different'))
+    def bsample_block(self, wf, n_res=4):
+        bdraw_next = torch.compile(torch.vmap(lambda x: draw_next(wf, x, n_flip=1, n_iter=2**4), randomness='different'))
         blocal_energy = torch.compile(torch.vmap(lambda x: self.local_energy(wf, x)))
-        vmgrad = torch.compile(torch.vmap(lambda x: derivatives(wf, x)))
+        # vmgrad = torch.compile(torch.vmap(lambda x: derivatives(wf, x)))
         vmagrad = torch.compile(torch.vmap(lambda x: autograd(wf, x)))
 
-        self.samples = bdraw_next(self.samples)
+        for _ in range(n_res):
+            self.samples = bdraw_next(self.samples)
         self.EL = blocal_energy(self.samples)
 
         self.OK = vmagrad(self.samples)
