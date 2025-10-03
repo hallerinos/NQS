86ca7a1dc4477e34153a3b5d88fb99277824e859
Andreas Haller <andreas.haller@uni.lu>
2025-10-03 16:50:23 +0200

    small changes in main

--- new/main.py
+++ new/main.py
@@ -7,7 +7,7 @@ import tqdm
 import numpy as np
 
 if __name__ == "__main__":
-    torch.set_default_dtype(torch.float32)
+    torch.set_default_dtype(torch.double)
     torch.set_default_device('cuda')
     torch.manual_seed(0)
 
@@ -18,7 +18,7 @@ if __name__ == "__main__":
 
     ic(n_spin, Ns, model.n_param)
 
-    sampler = sampler(model, Ns, local_energy=TFIM)
+    sampler = sampler(model, Ns, local_energy=lambda model, x: TFIM(model, x, J=-1, h=-1))
     sampler.warmup(n_res=n_spin//4, max_iter=2**10)
     tbar = tqdm.trange(2**8)
     for epoch in tbar:
@@ -26,7 +26,7 @@ if __name__ == "__main__":
 
         Eav = sampler.EL.mean()
         epsbar = (sampler.EL - Eav) / Ns**0.5
-        Evar = torch.conj(epsbar) @ epsbar
+        Evar = (torch.conj(epsbar) @ epsbar).mean()
 
         f = lambda primals: torch.func.functional_call(model, primals, sampler.samples)
         _, vjp = torch.func.vjp(f, model.state_dict())
@@ -35,10 +35,12 @@ if __name__ == "__main__":
         fav = lambda primals: torch.func.functional_call(model, primals, sampler.samples).mean()
         _, vjpav = torch.func.vjp(fav, model.state_dict())
         vjpavres = vjpav(Eav)[0]
+
         for (k, v) in vjpres.items():
             dTh = v/Ns - vjpavres[k]
             sampler.model.state_dict()[k] += - eta  * dTh
+
         Edens = Eav / n_spin
         tbar.set_description(
-                f"E/N: {np.round(Edens.cpu(), decimals=4)}, \u03C3: {np.round(Evar.mean().cpu(), decimals=4)}"
+                f"E/N: {np.round(Edens.cpu(), decimals=4)}, \u03C3: {np.round(Evar.cpu(), decimals=4)}"
             )
